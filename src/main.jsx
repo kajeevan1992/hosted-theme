19db550b66d94bd970507f38f231cca56d4b2b8a
kajeevan1992 <kajeevan1992@icloud.com>
2026-05-01 08:03:04 +0100

    wire hosted theme to internal storefront adapter

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import installStorefrontAdapter from './storefrontAdapter'
+
+// Install SaaS adapter BEFORE app mounts
+installStorefrontAdapter()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
