97fe4538f444b0058c6941bd03ede067a01bcea2
kajeevan1992 <kajeevan1992@icloud.com>
2026-05-16 02:41:45 +0100

    Switch product renderer back to original HOLO product template

--- src/storefront/pages/ProductPageRenderer.jsx
+++ src/storefront/pages/ProductPageRenderer.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import createProductPagePayload from '../data/productPagePayload';
-import ProductPageComposerV2 from './ProductPageComposerV2';
+import HoloOriginalProductTemplate from './HoloOriginalProductTemplate';
 
 export function ProductPageRenderer({ product = {} }) {
-  const payload = createProductPagePayload(product);
-
-  return (
-    <ProductPageComposerV2
-      page={payload}
-      product={product}
-      context={{
-        pageType: 'product',
-        product,
-      }}
-    />
-  );
+  return <HoloOriginalProductTemplate product={product} />;
 }
 
 export default ProductPageRenderer;
