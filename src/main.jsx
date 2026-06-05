abbfd861372782bfdd1a709e560d1951cf445f97
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-05 22:42:29 +0100

    Wire hosted theme SEO metadata manager

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './ConnectedApp'
 import './index.css'
 import installStorefrontAdapter from './storefrontAdapter'
+import { initStorefrontSeo } from './seo/storefrontSeo'
 
 // Install SaaS adapter BEFORE app mounts
 installStorefrontAdapter()
+initStorefrontSeo()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
