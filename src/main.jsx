bba794fbc276294c9a5f7b26b381d800d039c831
kajeevan1992 <kajeevan1992@icloud.com>
2026-06-07 20:58:53 +0100

    Initialize GA4 analytics when configured

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './ConnectedApp'
 import './index.css'
 import installStorefrontAdapter from './storefrontAdapter'
 import { initStorefrontSeo } from './seo/storefrontSeo'
+import { initGa4Analytics } from './analytics/ga4'
 
 // Install SaaS adapter BEFORE app mounts
 installStorefrontAdapter()
 initStorefrontSeo()
+initGa4Analytics()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
